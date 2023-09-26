cf077fb222e4a74f9fbfe44680d43f0f55a44b47
Abhishek Pradhan <78353414+Abhishek-723@users.noreply.github.com>
2023-09-26 23:50:47 +0530

    Added profile image

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,6 +1,7 @@
 import { Avatar } from "@mui/material";
 import React, { useState } from "react";
 import classnames from "classnames";
+import profilePhoto from "../Images/Profile1.jpg";
 
 const Navbar = () => {
   const [selectedItem, setSelectedItem] = useState(0);
@@ -8,11 +9,11 @@ const Navbar = () => {
   return (
     <div className="w-full flex px-20 py-8 justify-between items-center">
       <div className="flex w-max items-center">
-        <Avatar alt="" src="/static/images/avatar/1.jpg" />
+        <Avatar alt="" src={profilePhoto} />
         <p className="text-zinc-300 text-2xl font-bold ml-2">ABHISHEK</p>
         <div className="w-[6px] h-[6px] bg-green-600 mt-[18px] ml-[4px] rounded-full"></div>
       </div>
-      <div className="w-[46%] rounded-xl border border-slate-600 px-2 py-1 flex w-full justify-between items-center text-white bg-gray-900">
+      <div className="w-1/2 rounded-xl border border-slate-600 px-2 py-1 flex w-full justify-between items-center text-white bg-gray-900">
         {navItems.map((item, index) => (
           <div
             className={classnames(
