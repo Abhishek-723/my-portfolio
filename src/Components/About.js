5268b79e2acf94155a9ff935994aa355fa5413e5
Abhishek Pradhan <78353414+Abhishek-723@users.noreply.github.com>
2023-10-09 00:18:01 +0530

    Added responsive about section

--- src/Components/About.js
+++ src/Components/About.js
@@ -6,11 +6,31 @@ import CN from "../Images/codingninjas.webp";
 import Deloitte from "../Images/deloitte.webp";
 import WorkTwoToneIcon from "@mui/icons-material/WorkTwoTone";
 import SchoolIcon from "@mui/icons-material/School";
+import MBS from "../Images/MBSLogo.jfif";
+import OUTR from "../Images/OUTRLogo.png";
 
 const About = () => {
   const Education = [
     {
       schoolName: "Odisha University of Technology and Research",
+      Degree: "B.Tech CSE",
+      startDate: "Oct 2020",
+      endDate: "Present",
+      Image: OUTR,
+    },
+    {
+      schoolName: "MBS Public School, Bhubaneswar",
+      Degree: "Class XII",
+      startDate: "May 2017",
+      endDate: "Apr 2019",
+      Image: MBS,
+    },
+    {
+      schoolName: "MBS Public School, Bhubaneswar",
+      Degree: "Class XII",
+      startDate: "May 2016",
+      endDate: "Apr 2017",
+      Image: MBS,
     },
   ];
   const experience = [
@@ -52,9 +72,9 @@ const About = () => {
   ];
   return (
     <div className="lg:px-20 px-8 py-12 text-gray-300 font-semibold">
-      <p className="text-5xl font-bold mb-16">About</p>
-      <div className="flex justify-around">
-        <div className="w-1/3 border border-slate-800 rounded-xl">
+      <p className="text-5xl font-bold mb-16 md:text-left text-center">About</p>
+      <div className="flex justify-center flex-wrap md:justify-around">
+        <div className="lg:w-1/3 w-3/5 min-w-[300px] border border-slate-800 rounded-xl mb-6">
           <div className="flex px-4 py-4 border-b border-slate-800 items-center">
             <div className="w-10 h-10 rounded-full bg-gray-800">
               <WorkTwoToneIcon className="ml-2 mt-2" />
@@ -84,7 +104,7 @@ const About = () => {
             ))}
           </div>
         </div>
-        <div className="w-1/3 border border-slate-800 rounded-xl">
+        <div className="lg:w-1/3 w-3/5 min-w-[300px] border border-slate-800 rounded-xl mb-6">
           <div className="flex px-4 py-4 border-b border-slate-800 items-center">
             <div className="w-10 h-10 rounded-full bg-gray-800">
               <SchoolIcon className="ml-2 mt-2" />
@@ -92,7 +112,7 @@ const About = () => {
             <p className="ml-4 text-md">Education</p>
           </div>
           <div className="my-8">
-            {experience.map((data) => (
+            {Education.map((data) => (
               <div className="px-4 py-2">
                 <div className="flex w-full">
                   <img
@@ -101,9 +121,9 @@ const About = () => {
                     className="w-10 h-10 rounded-full"
                   />
                   <div className="ml-4 w-4/5">
-                    <p className="text-sm mb-1">{data.Role}</p>
+                    <p className="text-sm mb-1">{data.Degree}</p>
                     <div className="text-xs flex justify-between">
-                      <p>{data.Company}</p>
+                      <p>{data.schoolName}</p>
                       <p className="text-gray-600">
                         {data.startDate} - {data.endDate}
                       </p>
